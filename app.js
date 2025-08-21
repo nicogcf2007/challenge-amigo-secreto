c0c8c26982828caf6558e92d8e21285f974ab468
Nicolas Rivera <nicogcf2007@gmail.com>
2025-08-21 09:21:56 -0500

    Logica del challenge creada en JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Definimos lista de amigos
+var amigos = [];
+// Actualizamos la lista de amigos
+const actualizarAmigos = () => {
+    let listaAmigosInput = document.getElementById("listaAmigos");
+    listaAmigosInput.innerHTML = "Tus amigos son:";
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigosInput.appendChild(li);
+    });
+}
+// Agregamos amigo
+const agregarAmigo = () => { 
+    let amigoInput = document.getElementById("amigo");
+    let amigoValue = amigoInput.value;
+    if (amigoValue === "") {
+        alert("Por favor, ingresa un nombre válido");
+        return;
+    }
+    amigos.push(amigoValue);
+    console.log(amigos);
+    amigoInput.value = "";
+    actualizarAmigos();
+}
+// Sorteamos amigo
+const sortearAmigo = () => {
+    let numero = Math.floor(Math.random() * amigos.length);
+    let seleccionado = amigos[numero];
+    console.log(seleccionado);
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Tu amigo secreto es: ${seleccionado}`;
+}
